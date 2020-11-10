75457084364d188471779a399623322232c0480b
rexvel <teteriv22@gmail.com>
2020-11-10 21:18:20 +0200

    feat: api implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,64 @@
 import Container from '@material-ui/core/Container';
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "react-toastify/dist/ReactToastify.css";
 
 
 
 const App = () => {
-
-    useEffect(() => {
-
-    }, []);
+  const [pokemonArray, setPokemons] = useState([]);
+  const [beerArray, setBeerArray] = useState([]);
 
 
+  async function getPokemon():Promise<any> {
+    try {
+      let result = await fetch("https://pokeapi.co/api/v2/ability/?limit=50&offset=50/", {
+        method: "GET"
+      });
+      const { results } = await result.json()
+      //TODO make suitable function for delegating state management
+      const pokemons = results.sort(() => Math.random() - Math.random()).slice(0, 5)
+      setPokemons(pokemons)
 
-    return (
-        <Container component="main" maxWidth="xs">
-          init
-        </Container>
-    );
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async function getBeer():Promise<any> {
+    try {
+      let res = await fetch("https://api.punkapi.com/v2/beers", {
+        method: "GET"
+      });
+
+      const results = await res.json()
+
+      //TODO make suitable function for delegating state management
+      const beer = results.sort(() => Math.random() - Math.random()).slice(0, 5)
+      setBeerArray(beer)
+    } catch (err) {
+    }
+
+  }
+
+
+  useEffect(() => {
+    getPokemon()
+    getBeer();
+
+
+  }, []);
+
+
+  console.log(pokemonArray)
+  console.log(beerArray)
+
+
+
+  return (
+    <Container component="main" maxWidth="xs">
+      init
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/Item/Items.tsx
@@ -0,0 +1,13 @@
+import Container from '@material-ui/core/Container';
+import React from 'react';
+
+
+export const Item = () => {
+
+
+  return (
+    <Container component="main" maxWidth="xs">
+      init
+    </Container>
+  );
+}
