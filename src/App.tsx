20d2f3c329ce19f78aadd2c5fe06c592021cdf35
rexvel <teteriv22@gmail.com>
2020-11-10 15:43:00 +0200

    feat: init setup

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Container from '@material-ui/core/Container';
+import React, { useEffect } from "react";
+import "react-toastify/dist/ReactToastify.css";
+
+
+
+const App = () => {
+
+    useEffect(() => {
+
+    }, []);
+
+
+
+    return (
+        <Container component="main" maxWidth="xs">
+          init
+        </Container>
+    );
+}
+
+export default App;
